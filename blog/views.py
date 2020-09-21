698db8bda87f5d8d8e0d8de1d20468def060fea2
Magali Vangkeosay <Tylosten@users.noreply.github.com>
2020-09-21 15:00:45 +0200

    Add login and drafts

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,18 @@ from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+@login_required
+def draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_at")
+    return render(request, "blog/draft_list.html", {"posts": posts})
 
 
 def post_detail(request, pk):
@@ -25,7 +31,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
     else:
@@ -41,9 +46,22 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, "blog/post_edit.html", {"form": form})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect("post_detail", pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect("post_list")
